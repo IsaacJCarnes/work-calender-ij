9b51d0212774804d0a1b6019e75b2dee1ef03d1c
Isaac Joshua <isaacjcarnes@gmail.com>
2021-10-04 19:08:25 -0400

    currentDay now shows the date in 'dddd MM Do' format

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,6 @@
 console.log(moment());
+console.log(moment("getHours"));
+console.log(moment().utcOffset('+2000').format('YYYY-MM-DD HH:mm'));
+const container = document.getElementById("container");
+const currentDay = document.getElementById("currentDay");
+currentDay.textContent = moment().format("dddd, MMMM Do");
