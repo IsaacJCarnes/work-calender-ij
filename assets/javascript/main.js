545d096cd2959c5cdae13d5103a2837b95101959
Isaac Joshua <isaacjcarnes@gmail.com>
2021-10-04 22:06:55 -0400

    JS now adds an area for each hour of the work. Button needs to save text to local storage

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,43 @@
 console.log(moment());
-console.log(moment("getHours"));
 console.log(moment().utcOffset('+2000').format('YYYY-MM-DD HH:mm'));
 const container = document.getElementById("container");
 const currentDay = document.getElementById("currentDay");
 currentDay.textContent = moment().format("dddd, MMMM Do");
+
+const workDay = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+function base24To12(time){
+    if(time < 12){
+        return time + "AM";
+    } else if(time == 12){
+        return time + "PM";
+    } else {
+        return time%12 + "PM";
+    }
+}
+
+for(i = 0; i < workDay.length; i++){
+    var timeBlock = document.createElement("div");
+    timeBlock.classList = "time-block row";
+
+    var hour = document.createElement("div");
+    hour.classList = "hour col-1";
+    hour.textContent = base24To12(workDay[i]);
+
+    var taskBlock = document.createElement("textarea");
+    if(workDay[i] < Number(moment().format("HH"))){
+        taskBlock.classList = "past col-6";
+    } else if(workDay[i] == Number(moment().format("HH"))){
+        taskBlock.classList = "present col-6";
+    } else {
+        taskBlock.classList = "future col-6";
+    }
+
+    var saveBtn = document.createElement("button");
+    saveBtn.classList = "saveBtn col-1";
+
+    timeBlock.appendChild(hour);
+    timeBlock.appendChild(taskBlock);
+    timeBlock.appendChild(saveBtn);
+    container.appendChild(timeBlock);
+}
