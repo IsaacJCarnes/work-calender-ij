e41c74247a6eca92818f77e849d50b4084e3b669
Isaac Joshua <isaacjcarnes@gmail.com>
2021-10-06 20:57:20 -0400

    Added comments to js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,3 @@
-console.log(moment());
-console.log(moment().utcOffset('+2000').format('YYYY-MM-DD HH:mm'));
 const container = document.getElementById("container");
 const currentDay = document.getElementById("currentDay");
 currentDay.textContent = moment().format("dddd, MMMM Do");
@@ -9,19 +7,18 @@ var taskMessages = ["", "", "", "", "", "", "", "", ""];
 var stringMessages = localStorage.getItem("messages");
 
 if(stringMessages == null){
-    updateDataset(); //Creates string version of taskMessages in localstorage
+    updateStorage(); //Creates string version of taskMessages in localstorage
 } else {
     //Sets message array to their local storage counterparts
     taskMessages = JSON.parse(stringMessages);
 }
 
-function updateDataset(){
+function updateStorage(){ //updates stringMessages and localStorage to reflect taskMessages
     stringMessages = JSON.stringify(taskMessages);
     localStorage.setItem("messages", stringMessages);
-    console.log(localStorage.getItem('messages'));
 }
 
-function base24To12(time){
+function base24To12(time){ //Changes base 24 time to base 12 time with 'AM' or 'PM'
     if(time < 12){
         return time + "AM";
     } else if(time == 12){
@@ -33,30 +30,32 @@ function base24To12(time){
 
 for(i = 0; i < workDay.length; i++){
     var timeBlock = document.createElement("div");
-    timeBlock.classList = "time-block row";
+    timeBlock.classList = "time-block row"; //css class and width data
 
     var hour = document.createElement("div");
-    hour.classList = "hour col-1";
+    hour.classList = "hour col-1"; //css class and width data
     hour.textContent = base24To12(workDay[i]);
 
-    var taskBlock = document.createElement("textarea");
-    if(workDay[i] < Number(moment().format("HH"))){
-        taskBlock.classList = "past col-6";
+    var taskBlock = document.createElement("textarea"); //textarea so it can be written in
+    if(workDay[i] < Number(moment().format("HH"))){ //Displays hour with suffix
+        taskBlock.classList = "past col-6"; //css class and width data
     } else if(workDay[i] == Number(moment().format("HH"))){
-        taskBlock.classList = "present col-6";
+        taskBlock.classList = "present col-6"; //css class and width data
     } else {
-        taskBlock.classList = "future col-6";
+        taskBlock.classList = "future col-6"; //css class and width data
     }
     taskBlock.value = taskMessages[i];
-    taskBlock.id = i;
+    taskBlock.id = i; //id set so it can be refrenced later
 
     var saveBtn = document.createElement("button");
-    saveBtn.classList = "saveBtn col-1";
+    saveBtn.classList = "saveBtn col-1"; //css class and width data
     saveBtn.dataset.position = i;
 
     saveBtn.onclick = function(){
+        //Changes the taskMessages array to hold the new text
         taskMessages[this.dataset.position] = document.getElementById(this.dataset.position).value;
-        updateDataset();
+        //Update localstorage
+        updateStorage();
     };
 
     timeBlock.appendChild(hour);
